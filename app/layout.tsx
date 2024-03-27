4416cb37c7780aab1c260c028ce2213ab72f1f0d
ashutoshlohani <ashulohani97@outlook.com>
2024-03-27 22:37:02 +0530

    font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import '@/app/ui/global.css';
+import { dm_sans } from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${dm_sans.className} antialiased`}>{children}</body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { DM_Sans, Lusitana } from 'next/font/google';
+
+export const dm_sans = DM_Sans({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
